49b20cea02191a63e34ff70b8747ae5cd595c073
John Chen <john.bm.chen@gmail.com>
2021-10-29 20:11:26 +1300

    feat: run script on shortcut

--- background.js
+++ background.js
@@ -1,15 +1,45 @@
-console.log("background script running");
-
+/**
+ * Event Listeners
+ */
 chrome.commands.onCommand.addListener((command) => {
   switch (command) {
-      case "open-palette":
-          openPalette();
-          break;
-      default:
-          break;
+    case "open-palette":
+      openPalette();
+      break;
+    default:
+      break;
   }
 });
 
-function openPalette() {
-    console.log("palette should open")
+/**
+ * Callback functions
+ */
+
+async function openPalette() {
+  console.log("palette should open");
+  var tab = await getCurrentTab();
+  console.log(tab);
+
+  if (tab == 'undefined' || tab.url == 'chrome://*') {
+      console.log("Invalid tab. Unable to open palette on this tab");
+      return;
+  }
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: displayPalette
+  });
+}
+
+/**
+ * Helper functions
+ */
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+function displayPalette() {
+  console.log("Palette to be displayed")
 }
