bb566cfd989a7ecffcc6ddb98c9ad7ee69873a55
Prabhnoor Singh <jattdhillon2001@gmail.com>
2021-10-29 22:16:31 +1300

    feat: input box

--- background.js
+++ background.js
@@ -27,7 +27,7 @@ async function openPalette() {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: displayPalette,
+    function: textbox,
   });
 }
 
@@ -40,18 +40,25 @@ async function getCurrentTab() {
   return tab;
 }
 
-function displayPalette() {
+function textbox() {
   console.log("Palette to be displayed");
   document.body.innerHTML +=
     '<dialog style="border: none; border-radius: 24px; width: 20vw;">\
-<h2> Do you want to feed your cat?</h2><br>\
- <button style="margin-bottom: 10px; display: block; width: 100%; color: white; background-color: #ec759c; padding: 15px; border: none; outline: none; font-size: 24px; border-radius: 12px; height: 10%; font-family: Arial, Helvetica, sans-serif;" class="b1">Feed Meeee</button>\
- <button style="margin-bottom: 30px; display: block; width: 100%; color: black; background-color: pink; padding: 15px; border: none; outline: none; font-size: 24px; border-radius: 12px; height: 10%; font-family: Arial, Helvetica, sans-serif;" class="b2">Stay Productive :[</button>\
-<img src="https://media1.tenor.com/images/0bf0e81a71059873bf8185b16c2b349c/tenor.gif?itemid=13657700" style="display: block; margin-left: auto; margin-right: auto; width: 50%;" alt="noodle" />\
+<h2> Deal with workspace</h2><br>\
+<form>\
+  <label for="commandName">command: </label><br>\
+  <input type="text" id="commandName" name="commandName"><br>\
+</form>\
+ <img src="https://media1.tenor.com/images/0bf0e81a71059873bf8185b16c2b349c/tenor.gif?itemid=13657700" style="display: block; margin-left: auto; margin-right: auto; width: 50%;" alt="noodle" />\
 </dialog>\
 <canvas id="confetti-holder" style="position: fixed; top:0; left: 0; z-index: 3000"></canvas>\
 ';
 
   var dialog = document.querySelector("dialog");
+  dialog.addEventListener("submit", (e) => {
+    let toBeInserted = document.getElementById("commandName").value;
+    e.preventDefault();
+    dialog.innerHTML += `<p> ${toBeInserted} </p>`;
+  });
   dialog.showModal();
 }
