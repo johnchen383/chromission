f9a3cabeb4166d481b375e4dbc656080277b8503
Prabhnoor Singh <jattdhillon2001@gmail.com>
2021-10-30 16:23:50 +1300

    feat: input a list of websites and open them on demand

--- background.js
+++ background.js
@@ -10,6 +10,9 @@ chrome.commands.onCommand.addListener((command) => {
       break;
   }
 });
+// chrome.browserAction.onClicked.addListener(() => {
+//   chrome.tabs.create({ url: "https://www.google.com" });
+// });
 
 /**
  * Callback functions
@@ -39,26 +42,42 @@ async function getCurrentTab() {
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 }
-
 function textbox() {
+  var arrayOfWebsites = []; // this will be done through chrome.storage later on
   console.log("Palette to be displayed");
-  document.body.innerHTML +=
-    '<dialog style="border: none; border-radius: 24px; width: 20vw;">\
-<h2> Deal with workspace</h2><br>\
-<form>\
+  function addHTML() {
+    document.body.innerHTML +=
+      '<dialog style="border: none; border-radius: 24px; width: 20vw;">\
+  <h2> Deal with workspace</h2><br>\
+  <form id="myForm">\
   <label for="commandName">command: </label><br>\
   <input type="text" id="commandName" name="commandName"><br>\
-</form>\
- <img src="https://media1.tenor.com/images/0bf0e81a71059873bf8185b16c2b349c/tenor.gif?itemid=13657700" style="display: block; margin-left: auto; margin-right: auto; width: 50%;" alt="noodle" />\
-</dialog>\
-<canvas id="confetti-holder" style="position: fixed; top:0; left: 0; z-index: 3000"></canvas>\
-';
+  </form>\
+  <img src="https://media1.tenor.com/images/0bf0e81a71059873bf8185b16c2b349c/tenor.gif?itemid=13657700" style="display: block; margin-left: auto; margin-right: auto; width: 50%;" alt="noodle" />\
+  <button id="openTabs">Open Workspace!</button>\
+  <ul id="demo"> </ul>\
+  </dialog>\
+  <canvas id="confetti-holder" style="position: fixed; top:0; left: 0; z-index: 3000"></canvas>\
+  ';
+  }
 
-  var dialog = document.querySelector("dialog");
-  dialog.addEventListener("submit", (e) => {
-    let toBeInserted = document.getElementById("commandName").value;
+  // workspace
+
+  addHTML();
+  const form = document.getElementById("myForm");
+  const dialog = document.querySelector("dialog");
+  const openTabs = document.getElementById("openTabs");
+  form.addEventListener("submit", (e) => {
+    console.log(e.type);
     e.preventDefault();
-    dialog.innerHTML += `<p> ${toBeInserted} </p>`;
+    let toBeInserted = document.getElementById("commandName").value;
+    arrayOfWebsites.push(toBeInserted);
+    document.getElementById("demo").innerHTML += `<li> ${toBeInserted} </li>`;
+    document.getElementById("commandName").value = "";
+  });
+  openTabs.addEventListener("click", () => {
+    console.log("array", arrayOfWebsites);
+    arrayOfWebsites.map((websiteURL) => window.open(websiteURL));
   });
   dialog.showModal();
 }
