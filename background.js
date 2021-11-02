a214dee14e845c527f1398187ad5417d2dd21374
John Chen <john.bm.chen@gmail.com>
2021-11-03 10:24:51 +1300

    feat: improved styling of comman palette

--- background.js
+++ background.js
@@ -1,4 +1,16 @@
 /**
+ * Get form and tabs element from .html
+ */
+const form = document.getElementById("myForm");
+
+const input = document.getElementById("commandName");
+
+if (input != null) {
+  input.focus();
+  input.value = "> ";
+}
+
+/**
  * Get the url of the current tab
  * @returns tab url
  */
@@ -13,84 +25,79 @@ async function getCurrentTab() {
 }
 
 /**
- * Get form and tabs element from .html
- */
-const form = document.getElementById("myForm");
-
-/**
  * Add form handler
  */
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const toBeInserted = document.getElementById("commandName").value;
-  let [command, workspace] = toBeInserted.split(" ");
+if (form != null) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const toBeInserted = input.value.split("> ", 2)[1];
+    let [command, workspace] = toBeInserted.split(" ");
 
-  switch (command) {
-    /**
-     * Open all tabs from a particular workspace
-     */
-    case "open":
-      chrome.storage.sync.get([workspace], function (result) {
-        let allwebsites = result[workspace];
-
-        if (allwebsites === undefined){
-          console.log('unable to open workspace: ' + workspace);
-          return;
-        }
-
-        allwebsites.map((website) => window.open(website));
-
-      });
-      break;
-    /**
-     * Adds all tabs of active window to a particular workspace
-     */
-    case "addAll": //for now to make them different - could change them later if wanted
-      TODO:
-      break;
-    /**
-     * Adds active tab to a particular workspace
-     */
-    case "add":
-      getCurrentTab().then((tab) => {
+    switch (command) {
+      /**
+       * Open all tabs from a particular workspace
+       */
+      case "open":
         chrome.storage.sync.get([workspace], function (result) {
-          let arrayOfWebsites = [tab];
+          let allwebsites = result[workspace];
 
-          if (result[workspace] !== undefined){
-            arrayOfWebsites.push(...result[workspace]);
+          if (allwebsites === undefined) {
+            console.log("unable to open workspace: " + workspace);
+            return;
           }
-         
-          chrome.storage.sync.set(
-            { [workspace]: arrayOfWebsites },
-            function () {
-              console.log("Value is set to " + arrayOfWebsites);
-            }
-          );
+
+          allwebsites.map((website) => window.open(website));
         });
-      });
-      break;
-    /**
-     * Removes active tab from a particular workspace
-     */
-    case "remove":
-      break;
-    /**
-     * Close all tabs which match a particular workspace
-     */
-    case "close":
-      break;
-    /**
-     * Closes all tabs and resets with a new tab
-     */
-    case "reset":
-      break;
-    /**
-     * Deletes a particular workspace
-     */
-    case "delete":
-      break;
-      
-    default:
-      "INVALID KEY";
-  }
-});
+        break;
+      /**
+       * Adds all tabs of active window to a particular workspace
+       */
+      case "addAll": //for now to make them different - could change them later if wanted
+        TODO: break;
+      /**
+       * Adds active tab to a particular workspace
+       */
+      case "add":
+        getCurrentTab().then((tab) => {
+          chrome.storage.sync.get([workspace], function (result) {
+            let arrayOfWebsites = [tab];
+
+            if (result[workspace] !== undefined) {
+              arrayOfWebsites.push(...result[workspace]);
+            }
+
+            chrome.storage.sync.set(
+              { [workspace]: arrayOfWebsites },
+              function () {
+                console.log("Value is set to " + arrayOfWebsites);
+              }
+            );
+          });
+        });
+        break;
+      /**
+       * Removes active tab from a particular workspace
+       */
+      case "remove":
+        break;
+      /**
+       * Close all tabs which match a particular workspace
+       */
+      case "close":
+        break;
+      /**
+       * Closes all tabs and resets with a new tab
+       */
+      case "reset":
+        break;
+      /**
+       * Deletes a particular workspace
+       */
+      case "delete":
+        break;
+
+      default:
+        "INVALID KEY";
+    }
+  });
+}
