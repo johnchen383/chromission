381606ae7defadb2009efd20b7e6611bcf81f080
Prabhnoor Singh <jattdhillon2001@gmail.com>
2021-11-14 16:03:44 +1300

    fix: no var command needed hehe

--- background.js
+++ background.js
@@ -26,9 +26,8 @@ const injectable = document.getElementById("injectable");
 const helpText = document.getElementById("help");
 const secondaryText = document.getElementById("secondary");
 const gif = document.getElementById("texting-gif");
-var command = "";
 var arrayOfCommands = [];
-var indexOfCommand = 1;
+var indexOfCommand = 0;
 const allCommands = [
   "add",
   "open",
@@ -54,9 +53,9 @@ const midPromptVals = [
 
 if (input !== null) {
   input.onkeydown = (e) => {
+    const command = input.value.split("> ", 2)[1].split(" ")[0];
     addInjectableText("type 'help' for list of commands!");
     toggleSecondaryVisibility(false);
-    console.log(e.key);
     if (command === "hel" && e.key === "p") {
       input.style.color = "turquoise";
     }
@@ -96,49 +95,34 @@ if (input !== null) {
           input.style.color = "gold";
           break;
       }
-    }
-    if (e.ctrlKey && e.key === "Backspace") {
-      //e.preventDefault();
-      console.log("herrrrrrrrrrrrr");
-      command = "";
     } else if (e.key === "Backspace") {
-      // console.log(command, input.value.length);
-
       if (input.value.length >= 3) {
-        command = command.substring(0, command.length - 1);
       } else {
         e.preventDefault();
         resetInputField();
       }
     } else if (e.key === "Enter") {
       arrayOfCommands.push(command);
-      command = "";
-    } else if (e.key.length === 1) {
-      // console.log("log", /[a-zA-Z]/.test(e.key));
-      command += e.key;
+      console.log("enter", arrayOfCommands);
     } else if (e.key === "ArrowUp") {
       e.preventDefault();
       if (arrayOfCommands.length > 0) {
-        let indexToGet = arrayOfCommands.length - indexOfCommand;
-        command = arrayOfCommands[indexToGet];
+        let indexToGet = arrayOfCommands.length - indexOfCommand - 1;
         input.value = "> " + arrayOfCommands[indexToGet];
-        if (indexOfCommand < arrayOfCommands.length) {
+        if (indexOfCommand < arrayOfCommands.length - 1) {
           indexOfCommand += 1;
         }
       }
-      // console.log(command);
     } else if (e.key === "ArrowDown") {
-      if (indexOfCommand > 1) {
+      if (indexOfCommand >= 1) {
         indexOfCommand -= 1;
 
-        let indexToGet = arrayOfCommands.length - indexOfCommand;
+        let indexToGet = arrayOfCommands.length - indexOfCommand - 1;
         if (arrayOfCommands[indexToGet] != undefined) {
           input.value = "> " + arrayOfCommands[indexToGet];
-          command = arrayOfCommands[indexToGet];
         }
       } else {
         input.value = "> ";
-        command = "";
       }
     } else if (e.key === "Tab") {
       e.preventDefault();
@@ -152,16 +136,12 @@ if (input !== null) {
       });
       if (changedCommand !== undefined) {
         input.value = "> " + changedCommand;
-        command = changedCommand;
-
         for (i = 0; i < 3; i++) {
-          console.log(command);
           if (midPrompts[i] == command) {
             addInjectableText(midPromptVals[i]);
           }
         }
       }
-      console.log(command);
     }
   };
 }
@@ -275,7 +255,6 @@ if (form != null) {
     const toBeInserted = input.value.split("> ", 2)[1];
     let [command, workspace] = toBeInserted.split(" ");
     injectable.style.display = "none";
-    console.log("switch=", toBeInserted);
     switch (command) {
       /**
        * Open all tabs from a particular workspace
@@ -545,7 +524,7 @@ if (form != null) {
         callAPI("knock-knock")
           .then((res) => {
             const joke = res.data[0];
-            console.log(joke);
+            // console.log(joke);
             addInjectableText(joke.setup);
             addSecondaryText(joke.punchline);
             resetInputField();
@@ -563,7 +542,7 @@ if (form != null) {
         callAPI("general")
           .then((res) => {
             const joke = res.data[0];
-            console.log(joke);
+            // console.log(joke);
             addInjectableText(joke.setup);
             addSecondaryText(joke.punchline);
             resetInputField();
